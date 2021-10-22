39d6076d01a1a4bd9d8c5bb5ce2aa712e4b8d086
saicharangcse@gmail.com <saicharangcse@gmail.com>
2021-10-22 14:50:41 +0545

    -a

--- app/components/page-intro/IntroButtons.js
+++ app/components/page-intro/IntroButtons.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import { Button } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+
+const jss = makeStyles((theme) => ({
+   root: {
+      display: 'flex',
+      [theme.breakpoints.down('md')]: {
+         justifyContent: 'center',
+         alignItems: 'center'
+      }
+   }
+}));
 
 function IntroButtons() {
+   const styles = jss();
    return (
-      <div className={'d-flex justify-content-center'}>
+      <div className={styles.root}>
          <div className={'mr-2'}>
             <Button
                className={'text-white'}
